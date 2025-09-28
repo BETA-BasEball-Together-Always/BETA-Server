8c595b54b834d3657214d3b347ce525257f9f607
노영오 <nohyo05@naver.com>
2025-09-28 23:17:18 +0900

    메인 애플리케이션 클래스 설정 - JPA Auditing 활성화

--- main-server/src/main/java/com/beta/MainServerApplication.java
+++ main-server/src/main/java/com/beta/MainServerApplication.java
@@ -2,12 +2,12 @@ package com.beta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MainServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MainServerApplication.class, args);
     }
-
 }
