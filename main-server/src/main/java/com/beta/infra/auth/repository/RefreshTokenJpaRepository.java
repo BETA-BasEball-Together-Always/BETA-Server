accec72448ba5d4a935a9c6df1c4e31185c82498
노영오 <nohyo05@naver.com>
2025-10-19 15:11:14 +0900

    Token JPA Repository 구현
    - findByToken 추가

--- main-server/src/main/java/com/beta/infra/auth/repository/RefreshTokenJpaRepository.java
+++ main-server/src/main/java/com/beta/infra/auth/repository/RefreshTokenJpaRepository.java
@@ -4,9 +4,12 @@ import com.beta.infra.auth.entity.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 public interface RefreshTokenJpaRepository extends JpaRepository<RefreshTokenEntity, Long> {
 
     void deleteByUserId(Long userId);
+    
+    Optional<RefreshTokenEntity> findByToken(String token);
 }
